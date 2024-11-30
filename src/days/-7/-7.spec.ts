0708db0caa57c18e9a5dcd19d9e133932e324698
Elke Heymans <el.heymans@gmail.com>
2024-11-30 16:32:39 +0100

    Day -7 part 1

--- src/days/-7/-7.spec.ts
+++ src/days/-7/-7.spec.ts
@@ -1,37 +1,151 @@
 import { expect, test, describe } from "vitest";
-import { part1, part2 } from ".";
-import data from "./day01.txt?raw";
+import {
+  analyseSky,
+  buildSky,
+  Coordinate,
+  createIsInBoundsChecker,
+  DIRECTIONS,
+  findNextStar,
+  isInBounds,
+  isSameCoordinate,
+  part1,
+  takeNextCoordinate,
+} from ".";
+import data from "./day-7.txt?raw";
 
 describe("day -7", () => {
   describe("part 1", () => {
-    test("example", () => {
-      expect(part1(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"])).toBe(
-        142
-      );
+    test("actual", () => {
+      expect(part1(data.split("\n"))).toBe(416);
     });
-
-    test("real", () => {
-      expect(part1(data.split("\n"))).toBe(56465);
+    describe("examples", () => {
+      test("mini example", () => {
+        expect(part1(miniExample)).toBe(6);
+      });
+      test("mini mini example", () => {
+        expect(part1([".*.*.*.", "......."])).toBe(1);
+      });
+      test("Remcoder's example", () => {
+        expect(part1([".....", ".*...", "*.*..", ".*.*.", "..*.."])).toBe(2);
+      });
+      test("bigger example", () => {
+        expect(part1(biggerExample)).toBe(25);
+      });
     });
-  });
-
-  describe("part 2", () => {
-    test("example", () => {
-      expect(
-        part2([
-          "two1nine",
-          "eightwothree",
-          "abcone2threexyz",
-          "xtwone3four",
-          "4nineeightseven2",
-          "zoneight234",
-          "7pqrstsixteen",
-        ])
-      ).toBe(281);
-    });
-
-    test("real", () => {
-      expect(part2(data.split("\n"))).toBe(55902);
+    describe("helpers", () => {
+      test("takeNextCoordinate", () => {
+        expect(takeNextCoordinate([3, 4], DIRECTIONS.NORTH)).toStrictEqual([
+          3, 3,
+        ]);
+        expect(takeNextCoordinate([3, 4], DIRECTIONS.SOUTH)).toStrictEqual([
+          3, 5,
+        ]);
+        expect(takeNextCoordinate([3, 4], DIRECTIONS.WEST)).toStrictEqual([
+          2, 4,
+        ]);
+        expect(takeNextCoordinate([3, 4], DIRECTIONS.EAST)).toStrictEqual([
+          4, 4,
+        ]);
+      });
+      test("isSameCoordinate", () => {
+        expect(isSameCoordinate([3, 4], [3, 4])).toBeTruthy();
+        expect(isSameCoordinate([4, 4], [3, 4])).toBeFalsy();
+        expect(isSameCoordinate([3, 4], [4, 2])).toBeFalsy();
+      });
+      test("isInBounds", () => {
+        expect(isInBounds([3, 2], 5, 5)).toBeTruthy();
+        expect(isInBounds([0, 2], 5, 5)).toBeTruthy();
+        expect(isInBounds([3, -1], 5, 5)).toBeFalsy();
+      });
+      test("analyseSky", () => {
+        expect(analyseSky(miniExample)).toStrictEqual(miniExampleSky);
+      });
+      test("findNextStar", () => {
+        const isInBounds = createIsInBoundsChecker(5, 0);
+        expect(
+          findNextStar([1, 0], DIRECTIONS.NORTH, isInBounds, [
+            [1, 0],
+            [3, 0],
+          ])
+        ).toStrictEqual(false);
+        expect(
+          findNextStar([1, 0], DIRECTIONS.EAST, isInBounds, [
+            [1, 0],
+            [3, 0],
+          ])
+        ).toStrictEqual([3, 0]);
+        expect(
+          findNextStar([1, 0], DIRECTIONS.SOUTH, isInBounds, [
+            [1, 0],
+            [3, 0],
+          ])
+        ).toStrictEqual(false);
+        expect(
+          findNextStar([1, 0], DIRECTIONS.WEST, isInBounds, [
+            [1, 0],
+            [3, 0],
+          ])
+        ).toStrictEqual(false);
+      });
+      test("buildSky", () => {
+        const isInBounds = createIsInBoundsChecker(5, 0);
+        expect([
+          ...buildSky(
+            [
+              [0, 0],
+              [2, 0],
+              [1, 1],
+              [2, 1],
+              [3, 1],
+              [5, 1],
+            ],
+            isInBounds
+          ).entries(),
+        ]).toStrictEqual([
+          ["[0,0]", [[2, 0]]],
+          [
+            "[2,0]",
+            [
+              [2, 1],
+              [0, 0],
+            ],
+          ],
+          ["[1,1]", [[2, 1]]],
+          [
+            "[2,1]",
+            [
+              [2, 0],
+              [3, 1],
+              [1, 1],
+            ],
+          ],
+          ["[3,1]", [[2, 1]]],
+          ["[5,1]", []],
+        ]);
+      });
     });
   });
 });
+
+const miniExample = [".......", ".*.*.*.", ".......", ".*.*...", "......."];
+const miniExampleStars: Array<Coordinate> = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [1, 3],
+  [3, 3],
+];
+const miniExampleSky = [6, 4, miniExampleStars];
+
+const biggerExample = [
+  "....................",
+  "...........*..*.....",
+  "........*........*..",
+  "....*.....*....*....",
+  ".......*...*........",
+  "..*.*....*..........",
+  ".......*..*.........",
+  "...........*......*.",
+  "..*.*............*..",
+  "....................",
+];
--- src/days/-7/index.ts
+++ src/days/-7/index.ts
@@ -1,7 +1,126 @@
-export const part1 = (lines: Array<string>): number => {
-  return 1;
+export type Coordinate = [number, number];
+type Sky = [number, number, Array<Coordinate>];
+
+export const DIRECTIONS: { [direction: string]: Coordinate } = {
+  WEST: [-1, 0],
+  SOUTH: [0, 1],
+  EAST: [1, 0],
+  NORTH: [0, -1],
 };
 
-export const part2 = (lines: Array<string>): number => {
-  return 2;
+export const analyseSky = (lines: Array<string>): Sky => {
+  let currentY = 0;
+  let maxX = 0;
+  let maxY = 0;
+  let foundStars: Array<Coordinate> = [];
+  lines.forEach((line) => {
+    line.split("").forEach((maybeStar, currentX) => {
+      if (maybeStar === "*") {
+        foundStars.push([currentX, currentY]);
+      }
+      maxX = Math.max(maxX, currentX);
+    });
+    maxY = Math.max(maxY, currentY);
+    currentY++;
+  });
+  return [maxX, maxY, foundStars];
+};
+
+export const takeNextCoordinate = (
+  [cX, cY]: Coordinate,
+  [dX, dY]: Coordinate
+): Coordinate => [cX + dX, cY + dY];
+
+export const isSameCoordinate = (
+  [c1X, c1Y]: Coordinate,
+  [c2X, c2Y]: Coordinate
+): boolean => c1X === c2X && c1Y === c2Y;
+
+type isInBoundsFunction = (c: Coordinate) => boolean;
+export const isInBounds = (
+  [c1X, c1Y]: Coordinate,
+  maxX: number,
+  maxY: number
+) => c1X >= 0 && c1Y >= 0 && c1X <= maxX && c1Y <= maxY;
+
+export const createIsInBoundsChecker =
+  (maxX: number, maxY: number): isInBoundsFunction =>
+  (c: Coordinate) =>
+    isInBounds(c, maxX, maxY);
+
+export const findNextStar = (
+  star: Coordinate,
+  direction: Coordinate,
+  isInBounds: isInBoundsFunction,
+  stars: Array<Coordinate>
+): Coordinate | false => {
+  let currentCoordinate = star;
+  do {
+    currentCoordinate = takeNextCoordinate(currentCoordinate, direction);
+    if (stars.find((s) => isSameCoordinate(s, currentCoordinate))) {
+      return currentCoordinate;
+    }
+  } while (isInBounds(currentCoordinate));
+  return false;
+};
+
+export const buildSky = (
+  starCoordinates: Array<Coordinate>,
+  isInBounds: isInBoundsFunction
+): Map<string, Array<Coordinate>> => {
+  const result = new Map();
+  starCoordinates.forEach((coordinate) => {
+    result.set(
+      getKey(coordinate),
+      [
+        findNextStar(coordinate, DIRECTIONS.NORTH, isInBounds, starCoordinates),
+        findNextStar(coordinate, DIRECTIONS.EAST, isInBounds, starCoordinates),
+        findNextStar(coordinate, DIRECTIONS.SOUTH, isInBounds, starCoordinates),
+        findNextStar(coordinate, DIRECTIONS.WEST, isInBounds, starCoordinates),
+      ].filter(Boolean) as Array<Coordinate>
+    );
+  });
+  return result;
+};
+
+const getKey = JSON.stringify;
+
+export const searchSky = (
+  sky: Map<string, Array<Coordinate>>,
+  star: Coordinate
+): Array<Array<string>> => {
+  const visited = new Set();
+  const queue: Array<[Coordinate, Array<string>]> = [[star, [getKey(star)]]];
+  let result = [];
+
+  while (queue.length > 0) {
+    const [node, path] = queue.shift()!;
+    visited.add(node);
+    if (path.length === 3) {
+      result.push(path);
+    } else {
+      if (sky.has(getKey(node))) {
+        for (const neighbor of sky.get(getKey(node))!) {
+          if (!visited.has(neighbor)) {
+            queue.push([neighbor, [...new Set([getKey(neighbor), ...path])]]);
+          }
+        }
+      }
+    }
+  }
+  return result;
+};
+
+export const part1 = (lines: Array<string>): number => {
+  const [maxX, maxY, foundStars] = analyseSky(lines);
+  const isInBounds = createIsInBoundsChecker(maxX, maxY);
+  const sky = buildSky(foundStars, isInBounds);
+
+  const result = new Set();
+  foundStars.forEach((star) =>
+    searchSky(sky, star).forEach((consellation) =>
+      result.add(JSON.stringify(consellation.sort()))
+    )
+  );
+  return result.size;
 };
