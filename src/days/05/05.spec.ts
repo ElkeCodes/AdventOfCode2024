0fd8bf78f685e63438f384dd8ed4282ab7b8dc74
Elke Heymans <el.heymans@gmail.com>
2024-12-05 22:06:49 +0100

    Day 05

---
+++ src/days/05/05.spec.ts
@@ -0,0 +1,166 @@
+import { expect, test, describe } from "vitest";
+import {
+  grabCenter,
+  isCorrectlyOrdered,
+  parseData,
+  part1,
+  part2,
+  sortUpdate,
+} from ".";
+import data from "./05.txt?raw";
+
+describe("day 01", () => {
+  describe("part 1", () => {
+    test("actual", () => {
+      expect(part1(data.split("\n"))).toBe(5087);
+    });
+    test("example", () => {
+      expect(part1(example)).toBe(143);
+    });
+  });
+
+  describe("part 2", () => {
+    test("actual", () => {
+      expect(part2(data.split("\n"))).toBe(4971);
+    });
+    test("example", () => {
+      expect(part2(example)).toBe(123);
+    });
+  });
+
+  describe("helpers", () => {
+    test("parseData", () => {
+      expect(parseData(example)).toStrictEqual({
+        rules: exampleRules,
+        updates: [
+          [75, 47, 61, 53, 29],
+          [97, 61, 53, 29, 13],
+          [75, 29, 13],
+          [75, 97, 47, 61, 53],
+          [61, 13, 29],
+          [97, 13, 75, 29, 47],
+        ],
+      });
+    });
+    test("isCorrectlyOrdered", () => {
+      expect(
+        isCorrectlyOrdered([75, 47, 61, 53, 29], exampleRules)
+      ).toBeTruthy();
+      expect(
+        isCorrectlyOrdered([97, 61, 53, 29, 13], exampleRules)
+      ).toBeTruthy();
+      expect(isCorrectlyOrdered([75, 29, 13], exampleRules)).toBeTruthy();
+      expect(
+        isCorrectlyOrdered([75, 97, 47, 61, 53], exampleRules)
+      ).toBeFalsy();
+      expect(isCorrectlyOrdered([61, 13, 29], exampleRules)).toBeFalsy();
+      expect(
+        isCorrectlyOrdered([97, 13, 75, 29, 47], exampleRules)
+      ).toBeFalsy();
+    });
+    test("grabCenter", () => {
+      expect(grabCenter([1, 2, 3])).toBe(2);
+      expect(grabCenter([1, 2, 3, 4, 5])).toBe(3);
+    });
+    test("sortUpdate", () => {
+      expect(sortUpdate([75, 97, 47, 61, 53], exampleRules)).toStrictEqual([
+        97, 75, 47, 61, 53,
+      ]);
+      expect(sortUpdate([61, 13, 29], exampleRules)).toStrictEqual([
+        61, 29, 13,
+      ]);
+      expect(sortUpdate([97, 13, 75, 29, 47], exampleRules)).toStrictEqual([
+        97, 75, 47, 29, 13,
+      ]);
+    });
+  });
+});
+
+const exampleRules = [
+  { before: 47, after: 53 },
+  { before: 97, after: 13 },
+  { before: 97, after: 61 },
+  { before: 97, after: 47 },
+  { before: 75, after: 29 },
+  { before: 61, after: 13 },
+  { before: 75, after: 53 },
+  { before: 29, after: 13 },
+  { before: 97, after: 29 },
+  { before: 53, after: 29 },
+  { before: 61, after: 53 },
+  { before: 97, after: 53 },
+  { before: 61, after: 29 },
+  { before: 47, after: 13 },
+  { before: 75, after: 47 },
+  { before: 97, after: 75 },
+  { before: 47, after: 61 },
+  { before: 75, after: 61 },
+  { before: 47, after: 29 },
+  { before: 75, after: 13 },
+  { before: 53, after: 13 },
+];
+
+const example = [
+  "47|53",
+  "97|13",
+  "97|61",
+  "97|47",
+  "75|29",
+  "61|13",
+  "75|53",
+  "29|13",
+  "97|29",
+  "53|29",
+  "61|53",
+  "97|53",
+  "61|29",
+  "47|13",
+  "75|47",
+  "97|75",
+  "47|61",
+  "75|61",
+  "47|29",
+  "75|13",
+  "53|13",
+  "",
+  "75,47,61,53,29",
+  "97,61,53,29,13",
+  "75,29,13",
+  "75,97,47,61,53",
+  "61,13,29",
+  "97,13,75,29,47",
+];
+
+/**
+ 
+"29|13",
+"47|13",
+"47|29",
+"47|53",
+"47|61",
+"53|13",
+"53|29",
+"61|13",
+"61|29",
+"61|53",
+"75|13",
+"75|29",
+"75|47",
+"75|53",
+"75|61",
+"97|13",
+"97|29",
+"97|47",
+"97|53",
+"97|61",
+"97|75",
+
+
+
+29: 13
+47: 13, 29, 53, 61
+53: 13, 29
+61: 13, 29, 53
+75: 13, 29, 47, 53, 61
+97: 13, 29, 47, 53, 61, 75
+ */
---
+++ src/days/05/index.ts
@@ -0,0 +1,98 @@
+interface Rule {
+  before: number;
+  after: number;
+}
+type Update = Array<number>;
+
+export const parseData = (
+  lines: Array<string>
+): { rules: Array<Rule>; updates: Array<Update> } => {
+  let isParsingUpdates = false;
+  let updates: Array<Update> = [];
+  let rules: Array<Rule> = [];
+  lines.forEach((line) => {
+    if (line === "") {
+      isParsingUpdates = true;
+      return;
+    }
+    if (isParsingUpdates) {
+      updates.push(line.split(",").map((n) => parseInt(n, 10)));
+    } else {
+      const [before, after] = line.split("|").map((n) => parseInt(n, 10));
+      rules.push({ before, after });
+    }
+  });
+
+  return { rules, updates };
+};
+
+export const isPageCorrectlyOrdered = (
+  update: Update,
+  page: number,
+  pageIndex: number,
+  rules: Array<Rule>
+): boolean => {
+  const relevantBeforeRules = rules.filter(({ before }) => before === page);
+  const relevantAfterRules = rules.filter(({ after }) => after === page);
+  return (
+    relevantBeforeRules.every(({ after }) => {
+      const foundIndex = update.findIndex((x) => x === after);
+      return foundIndex === -1 || foundIndex > pageIndex;
+    }) &&
+    relevantAfterRules.every(({ before }) => {
+      const foundIndex = update.findIndex((x) => x === before);
+      return foundIndex === -1 || foundIndex < pageIndex;
+    })
+  );
+};
+
+export const isCorrectlyOrdered = (
+  update: Update,
+  rules: Array<Rule>
+): boolean => {
+  return update.every((page, pageIndex) =>
+    isPageCorrectlyOrdered(update, page, pageIndex, rules)
+  );
+};
+
+export const grabCenter = (update: Update): number => {
+  return update.at((update.length - 1) / 2)!;
+};
+
+export const sortUpdate = (update: Update, rules: Array<Rule>): Update => {
+  let result: Update = [update.at(0)!];
+  for (let pageIndex = 1; pageIndex < update.length; pageIndex++) {
+    let page = update.at(pageIndex)!;
+    let isInserted = false;
+    for (let insertIndex = 0; insertIndex < result.length; insertIndex++) {
+      let tempResult = [...result];
+      tempResult.splice(insertIndex, 0, page);
+      if (isCorrectlyOrdered(tempResult, rules)) {
+        result = tempResult;
+        isInserted = true;
+        break;
+      }
+    }
+    if (!isInserted) {
+      result.push(page);
+    }
+  }
+  return result;
+};
+
+export const part1 = (lines: Array<string>): number => {
+  const { rules, updates } = parseData(lines);
+
+  return updates
+    .filter((update) => isCorrectlyOrdered(update, rules))
+    .reduce((acc, update) => acc + grabCenter(update), 0);
+};
+
+export const part2 = (lines: Array<string>): number => {
+  const { rules, updates } = parseData(lines);
+
+  return updates
+    .filter((update) => !isCorrectlyOrdered(update, rules))
+    .map((update) => sortUpdate(update, rules))
+    .reduce((acc, update) => acc + grabCenter(update), 0);
+};
